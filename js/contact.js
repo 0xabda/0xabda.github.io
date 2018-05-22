142650078fb557587bdcae6567aac022eeada92e
Naim A <227396+naim94a@users.noreply.github.com>
2018-05-22 13:33:46 +0300

    Initial commit, new style

---
+++ js/contact.js
@@ -0,0 +1,50 @@
+(function(){
+    var pubKey = openpgp.key.readArmored('-----BEGIN PGP PUBLIC KEY BLOCK-----\n\nmQINBFoXUgABEADeXzmPuEWYOZoj9uNz/SiCRlW+0YhlsETeLV29ffdRde9RuF7P\nRhLQQZD2yw1Rp3X9HcYHEzSmLzY9PRcks7J/5NMs+jSAiaSuUJhlsoQfrxH7bjVV\nZqch08MBR4jEXeFlvjWbJggnvo7Tx8J92YLa+NFkbLbOA9CRjLJRocdqnQHXPWCn\n/fBhwgrihYeFW20F2KsxnFCwyUxtrDEK6P4STm3v79Fm/wawVmkJhQn6+P6VB83x\nZnXfaXakqlTai40Up9Igxs845IFRzh4+qoLJvvV4+4iGIRWVx6ObkjlXW6YrqPsV\n7L7OKnD0rDXk9m6/kq8aulD9sp+RG9JV1MmgeT1a4DLqCJRyc+UDO2NYZhcGs0DV\nU1eUYAScifQ8kgXcFoUBKsE+mvfFkXUR8Jgrpozr7HvR+xbDfnVC5s1OZ+dmt7mq\nPAXlYJ2PNANzhpFl5hZlb25wY4+SIwGv5Frp0EBCTv9zalP6asXMXEncl75CQK0D\nBKQ0QNGGwu+BjIRvKd3JwT5xUChlmPbWwyHXHfE3oU/8smj1QOZhSO/UiR3s/Nql\ncqFXz4T4dJ9kuiVmZERlJKMK9RHoAmQw3ibB5S5/4MWxJgdPzPqQ/rx9ZUUL5VGW\nOeQjbeNduC+qvTzs7E6gMVmFeqwYAIVRatLjztURA//EjYJl5R2fSmZqfQARAQAB\ntBhOYWltIEFiZGEgPG5haW1AYWJkYS5ubD6JAjUEEAEIACkFAloXUgIGCwkHCAMC\nCRCJGjrJu22CTAQVCAoCAxYCAQIZAQIbAwIeAQAA+bQP/RrClqRS6juui/SnpKXF\nKiG+Sej+CoBR+VMioiX3AnVC3AEFzjFHw9G+PZkKe/e8+oDKfF1MtQjK2ytPZ6t/\nLJQeQN0q0VQtW28EOw51+Fqkslocue/qtXhbVmiAL2Jj+v2jkdVuhBxH90dQ7oJs\nmNLdhASch/0CmX4sH43uOtZ2IrHw3/vwC66Vnig8wl/B3gFv93bpYQgEYmc+CVNb\nX6uYyF+9qOdyXrqvzfK6f1UAUjhxyskNXml5Ds1cgc3iGU7vnpHPjr96S8xle8Q+\nYqVktcgJ0o8LRBWkniCbbItg/MJdvZxdQQmp0KqomoCGskpMaKcLSbroGwdxPbHR\nSoY7DGR2lrKtT5g9NTUoWCbNjVWNpBIO+CT1CZIEDP7DktFYLnW3nTB3ehngj/qX\niu7AiVLccBxC9LtKEWZ2cK2T4PnMBVBMVO76J8FlNaXtcvln4r7fz2gXpvanAo1S\ns/+JH1jDorWCJ/5cPdxmVRthR8w3wTQH2UFuPH84iiNTg0VTp9cKO2OZRTT/YKV2\n3V1tllpSWdTMs27VjIryGOKeKZAnz19oUxW0+8srvTRRb/9taAR/8sOYMtYTkR4c\nOyQZpuPTrKNuArbPpOwOjoeadGKTcihfhjHrR4TjvExj1hwoyzVCRzpLuCqQw+Ea\ny+HIT1RqIlW+9I5ySCtKRL9EuQINBFoXUgABEAC10pkhDCt3i8c+UK9Cf9fYj8hz\nf7/Cc2RYhGhACsB3qoyeJ3Zj/PfQyMSAYgF65vJv7O5U2PI2GhUtO9AgA4btvMET\nrypuZBaH3qjB8HcLOLgkWFr8Iw3NJxVde0RgjpLkR9SzmhwfZiFzjkp/fEtt8klp\ncAluQveH5HAA5GkyMiYk2D54+t3NFgHKGAzlNCVxLuZpLYOsnh8f20flgmo0jH9V\nXQaEGz/1Imc2MIiOwXiXkNHffJFcIk3mBIDn5YFz3HMSwW5ZYHplhrPUqoFSeutZ\nmWIJgAnr86qVeyZ5E/FsVt5GKlRjGUGL627Upfh0ykDxdbzqmi4WxnkOqkauRXsl\n34Oj+kBDVNPxSCf85sYRJG4wj058QdQy/MUEbqZd2kDa6Y7GoVTImlAyJro/GvhX\nTAwQ/XjIPIr5nKexNnHIbgmi3xUgWUdwplkGj46TocH7DFp53ATjAHGXxoPtnLi/\nUT/5DxUHEf0GG579GPQRR+ShX0+hJ8/NsSV3Sq3noPzQHG2yzhDUE4ajeYOqdF0S\nzsj091y0u4duEhdmD8qKX+WNOc9JIkZMiXM1aR6fyQD5wIeEeewC2PKs34VIXokP\ncva1LOIt5ayhr/G5sdvyfKzHtq0Z3IdUEkM5YDzJHeZ1uzKMk7KpdceMCi2MPDCQ\nTdWNSLfwlvre9jHJUwARAQABiQIfBBgBCAATBQJaF1IDCRCJGjrJu22CTAIbDAAA\nqnAP/RuHNl1vCv1V5xvwBBUFZ3fPqEPrOivp9A05Y1LYC0X8uNe1GNGGtziCv2tl\ngwiZQk0vYBEXOB88pfQ7O0tWJ3zJH3BWQY5IngDC4jAB/xPefNBv8Im/0+5avCWx\nb5jWIU9TYL+0u90MCJo6vqHvqhoWy89J+uHGMFAZFGZoIt9IZxM7EQdSItXcQktc\n9G0Dsfld66APHAtZGObuh6o6EPhuMq3wHYMI6HQ25qFTzOeU4BGDStB2hxqUHXE2\nz1fl8p3n/Fy3D/32M4bO7tdTBwS4FXECiuXAolysATtQLJ6GsxRwQ0uYAI9S3AfD\n8lszjrf4Ke0SZpkRBcYibLB7E/d/e5aj5LhMHat4G6N1UaL5klyXc4Su16TT+jTi\nmY9MR1VJ+IX4Fa/cqIylgKBZ8hJcGgm7bAxAelZ26OJT4IfPbmhM5wHy1HNq6Mev\n7XlULnoajksV8ngI2s1/TQQ4wCvl845gTIi48GXjUehwVAZmlBQ8YpTpwFvXy+z4\nZnelgILyqqHDGdy/3aMT1r0w5w60l69T8s3bLTmZ6KBm28VPgSkZFKmuGdaPqk/7\nBqAs5vNt2bGhFSNr/k43NA8bvCw45usUd8DQRAjoRLhCqG5tfNUjzcPDjsZxPjG4\nDhicGMJiIdNdyWMbk0WIs4kTQBVZ+0KNm/3m3aNhjq/Hl4V8\n=VjRM\n-----END PGP PUBLIC KEY BLOCK-----');
+    openpgp.config.show_version = false;
+    openpgp.config.show_comment = false;
+    openpgp.config.aead_protect = true;
+    
+    function reset() {
+        $('form textarea[name=message]')[0].value = '';
+        $('form input[name=email]')[0].value = '';
+        $('form button[type=submit]')[0].disabled = false;
+    }
+
+    $("form").on("submit", function(e){
+        e.preventDefault();
+        $("form button[type=submit]")[0].disabled = true;
+        console.log('Encrypting...');
+        openpgp.encrypt({
+            data: $("textarea[name=message]")[0].value,
+            publicKeys: pubKey.keys
+        }).then(function(msg){
+            console.log('Sending...');
+            $.ajax({
+                url: 'https://formspree.io/naim@abda.nl',
+                method: 'POST',
+                data: {
+                    message: msg.data,
+                    _replyto: $('input[name=email]')[0].value,
+                    _format: 'plain'
+                },
+                dataType: 'json',
+                error: function(xhr, status, err){
+                    console.log(xhr, status, err);
+                    alert('Failed to send email: ' + err);
+                    reset();
+                },
+                success: function(data, status, xhr){
+                    reset();
+                    console.log(data);
+                    alert("Message sent successfully.");
+                }
+            });
+        }).catch(function(e){
+            console.log(xhr, status, err);
+            alert('Failed to send email: ' + err);
+            reset();
+        });
+    });
+    var jsm = $('#isjsenabled');
+    jsm.html(jsm.html() + ", after PGP encryption using key 0x" + pubKey.keys[0].primaryKey.fingerprint.substr(-16, 16));
+})()
